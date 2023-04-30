634d954d8efd4f0253a0b949abbac3d328c0d985
DDD-2023 <130794644+DDD-2023@users.noreply.github.com>
2023-04-30 23:09:46 +0300

    Add files via upload

---
+++ task27-3.py
@@ -0,0 +1,17 @@
+#todo:
+# Создайте лямбда функцию, которая принимает один параметр – строку.
+# Переводит все буквы в нижний регистр и переворачивает их в обратном порядке. Пример входа: ‘ACbdzYx’,
+# Вывод: 'xyzdbca'
+
+
+# str_in = 'ABCdef123'#.lower()
+str_in = input('Введите строку: ').lower()
+
+# str_end = ''.join(list(reversed(list(map(lambda x: str_lower[x], range(len(str_lower)))))))# Работает
+# print(str_end)@ Работает
+
+# str_end = ''.join(list(reversed(list(map(lambda x: str_lower[x], range(len(str_lower)))))))# Тест
+
+lam_fun = lambda x: print(''.join(list(reversed(list(x.lower())))))
+
+lam_fun(str_in)
